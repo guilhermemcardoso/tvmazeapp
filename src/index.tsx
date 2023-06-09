d880667e23ed628392a7e6f5040fecabedd6f212
Guilherme Cardoso <guilhermemunizcardoso@gmail.com>
2023-06-09 19:00:48 -0300

    chore: configured react query and react native dotenv

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React, {useEffect} from 'react';
 import {NativeBaseProvider, StatusBar} from 'native-base';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Router from '~/navigation';
 import {themes} from './theme';
 import {useSettings} from './hooks/use-settings';
 
 function App() {
   const {loadSettings, theme} = useSettings();
+  const queryClient = new QueryClient();
 
   useEffect(() => {
     loadSettings();
@@ -13,12 +15,14 @@ function App() {
 
   return (
     <NativeBaseProvider theme={themes[theme]}>
-      <StatusBar
-        translucent
-        barStyle={theme === 'dark' ? 'light-content' : 'dark-content'}
-        backgroundColor="transparent"
-      />
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <StatusBar
+          translucent
+          barStyle={theme === 'dark' ? 'light-content' : 'dark-content'}
+          backgroundColor="transparent"
+        />
+        <Router />
+      </QueryClientProvider>
     </NativeBaseProvider>
   );
 }
